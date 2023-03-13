db9b99c051c8d522ae2595e94375eb2bd0f4657a
BoostedBaboon <Rune@decuyper.info>
2023-03-13 21:36:47 +0100

    Gezever

---
+++ Rune/numpy-pandas/Gezever numpy.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_excel(r'C:\Users\rune\OneDrive\Documents\GitHub\Python-workshop\Python workshop (Responses) (1).xlsx')
+
+df.dropna(inplace=True)
+print(df.info())
