1a5be40e0a5ced52d21e1ceef20717843bf9b786
Mathieu Butenaerts <m.g.a.butenaerts@student.tudelft.nl>
2023-03-13 20:00:54 +0100

    Butti was here

---
+++ Arnaud/OOP/main.py
@@ -0,0 +1,14 @@
+from faculteitbar import Faculteitbar
+from Bier import Bier
+
+
+# MAAK FACULTEITSBAR AAN
+
+# MAAK BIER AAN
+
+# VOEG BIER TOE AAN HET ASSORTIMENT
+
+# VOEG 100 AANWEZIGEN TOE
+
+# VERLAAG HET AANTAL LEDEN MET 101
+
